12f250d81d1db43ffeca90d45ccf39a74e770e84
AlexTheGreat600 <shivanshsrivastava@gmail.com>
2025-06-01 18:03:28 +0530

    dwm: increment smart gaps

--- dwm/config.h
+++ dwm/config.h
@@ -11,7 +11,7 @@ static const unsigned int gappih         = 10;  /* horiz inner gap between windo
 static const unsigned int gappiv         = 10;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 20;  /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov         = 20;  /* vert outer gap between windows and screen edge */
-static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
+static const int smartgaps_fact          = 2;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
 static const int bar_height              = 30;   /* 0 means derive from font, >= 1 explicit height */
